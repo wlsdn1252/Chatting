84a7865d92c71abb55a3c086108c134099160099
강진우 <wlsdn1252@naver.com>
2023-05-04 03:25:01 +0900

    프래그먼트와 리사이클러뷰를 위한 클래스, 레이아웃파일 구현

---
+++ app/src/main/java/com/example/chatting/userlist/UserAdapter.kt
@@ -0,0 +1,4 @@
+package com.example.chatting.userlist
+
+class UserAdapter {
+}
---
+++ app/src/main/java/com/example/chatting/userlist/UserFragment.kt
@@ -0,0 +1,7 @@
+package com.example.chatting.userlist
+
+import androidx.fragment.app.Fragment
+
+class UserFragment:Fragment() {
+
+}
